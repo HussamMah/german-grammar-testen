23e93f680bf23ca865afa4391d838092620f69db
HussamMah <141561556+HussamMah@users.noreply.github.com>
2025-09-01 00:36:19 +0200

    Add files via upload

---
+++ pages/admin.js
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>لوحة الإدارة</h1>
+      <p>هنا يمكنك إضافة/تعديل الأسئلة واستعراض النتائج.</p>
+    </div>
+  );
+}
